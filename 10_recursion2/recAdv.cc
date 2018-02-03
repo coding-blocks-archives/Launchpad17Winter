5ab63b93262a262bb1d9a111af07b42f551eae7b
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-03 19:28:42 +0530

    Adv Rec

--- 10_recursion2/recAdv.cc
+++ 10_recursion2/recAdv.cc
@@ -31,11 +31,56 @@ void printCodes(char numCode[], int be, char ans[], int i){
     }
 }
 
+void printSubset(char str[], int be, char ans[], int i){
+    if (str[be] == '\0'){
+        ans[i] = '\0';
+        cout << ans << endl;
+        return;
+    }
+
+    printSubset(str, be + 1, ans, i);       // left
+    ans[i] = str[be];
+    printSubset(str, be + 1, ans, i + 1);   // selected
+}
+
+void printArr(int arr[], int k){
+    for (int i = 0; i < k; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void printFixedSizeSet(int n, int start, int k, int idx, int arr[]){
+    if (n < k){
+        return; // such arrangement is not possible
+    }
+
+    if (idx == k){
+        printArr(arr, k);
+        return;
+    }
+
+    for(int i = 0; i < n - k + 1 && start <= n; ++i){
+        arr[idx] = start;
+        printFixedSizeSet(n, start + 1, k, idx + 1, arr);
+        ++start;
+    }
+
+}
+
 int main(){
     char numCode[100];
     char ans[100] = "";
 
-    cin >> numCode;
+    // cin >> numCode;
 
-    printCodes(numCode, 0, ans, 0);   
+    // printCodes(numCode, 0, ans, 0);  
+
+    // char str[100]; cin >> str;
+    // printSubset(str, 0, ans, 0);
+
+    int arr[100];
+    int n, k; cin >> n >> k;
+    printFixedSizeSet(n, 1, k, 0, arr);  
 }
