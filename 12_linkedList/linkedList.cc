ef95091b6a9e9c7fac479cbc24e5d7071afcc8f2
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-10 18:02:38 +0530

    Linked List

---
+++ 12_linkedList/linkedList.cc
@@ -0,0 +1,53 @@
+// Deepak Aggarwal, deepak@codingblocks.com
+// https://github.com/aggarwaldeepak/
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+Node* insertAtEnd(Node* head, int x, Node* &tail){
+    if (head == NULL){
+        head = new Node(x);
+        tail = head;
+        return head;
+    }
+
+    tail->next = new Node(x);
+    tail = tail->next;
+    return head;
+}
+
+Node* createLL(){
+    int x;
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true){
+        cin >> x;
+        if (x == -1) break;
+        head = insertAtEnd(head, x, tail);
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    Node* cur = head;
+    while(cur != NULL){
+        cout << cur->data << "-->";
+        cur = cur->next;
+    }
+}
+
+int main(){
+    Node* head = createLL();
+    printLL(head);
+    return 0;
+}
