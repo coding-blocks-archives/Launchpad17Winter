9648af74baef34fb5dd2b3cc6d1d616126a8bcf1
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-24 15:34:30 +0530

    Sort List

---
+++ 15_stackQue/listQues.cc
@@ -0,0 +1,62 @@
+// Deepak Aggarwal, deepak@codingblocks.com
+// https://github.com/aggarwaldeepak/
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+Node* insertAtEnd(Node* head, int x, Node* &tail){
+    if (head == NULL){
+        head = new Node(x);
+        tail = head;
+        return head;
+    }
+
+    tail->next = new Node(x);
+    tail = tail->next;
+    return head;
+}
+
+Node* createLL(){
+    int x;
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true){
+        cin >> x;
+        if (x == -1) break;
+        head = insertAtEnd(head, x, tail);
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    Node* cur = head;
+    while(cur != NULL){
+        cout << cur->data << "-->";
+        cur = cur->next;
+    }
+    cout << endl;
+}
+
+Node* sortList(Node* head){
+    
+}
+
+int main(){
+    Node* head = createLL();
+    printLL(head);
+
+    Node * updatedList = sortList(head);
+    printLL(reversedList);
+
+    return 0;
+}
