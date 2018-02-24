b3481cea2a889ee3bc91fe9bf240ba5fc3424faf
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-24 19:19:00 +0530

    Stack

--- 15_stackQue/redundantParant.cc
+++ 15_stackQue/redundantParant.cc
@@ -1,10 +1,35 @@
 // Deepak Aggarwal, Coding Blocks
 // deepak@codingblocks.com
 #include <iostream>
+#include <stack>
 using namespace std;
 
-int cntRedundantParant(string& exp){
+bool isOperator(char c){
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
 
+int cntRedundantParant(string& exp){
+    int cnt = 0; 
+    stack<char> stk;
+
+    for(int i = 0; i < exp.size(); ++i){
+        // if opening bracket or operator do this...
+        if (exp[i] == '(' || isOperator(exp[i])){
+            stk.push(exp[i]);
+        }
+        // else if a closing bracket is found
+        else if (exp[i] == ')'){
+            bool opFound = false;
+            // pop until the opening bracket is found
+            while(stk.top() != '('){
+                if (stk.top() != '(') opFound = true;
+                stk.pop();
+            }
+            stk.pop();
+            if (opFound == false) ++cnt;
+        }
+    }
+    return cnt;
 }
 
 int main(){
--- 15_stackQue/windowMax.cc
+++ 15_stackQue/windowMax.cc
@@ -1,10 +1,38 @@
 // Deepak Aggarwal, Coding Blocks
 // deepak@codingblocks.com
 #include <iostream>
+#include <deque>
+#include <vector>
 using namespace std;
 
-vector<int> windowMax(vector<int>& v, int w){
+void specialPush(deque<int>& q, vector<int>& v, int i){
+    // weaker element cannot withstand me
+    while(q.empty() == false && q.back() < v[i]){
+        q.pop_back();
+    }
+    q.push_back(i);
+}
 
+
+vector<int> windowMax(vector<int>& v, int w){
+    deque<int> q;
+    vector<int> ans;
+
+    // create a window
+    for(int i = 0; i < w - 1; ++i){
+        specialPush(q, v, i);
+    }
+
+    int start = 0;
+    int endWindow = w - 1;
+    while(endWindow < v.size()){
+        specialPush(q, v, endWindow);
+        ans.push_back(v[q.front()]);
+        ++start;
+        ++endWindow;
+        if (q.front() < start) q.pop_front();
+    }
+    return ans;
 }
 
 int main(){
