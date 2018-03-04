0eec86105a50808a193cc35aeed68665725b5030
Deepak Aggarwal <daggarwal77@gmail.com>
2018-03-04 17:12:27 +0530

    best-k

--- 18_graphs/hashingQues.cc
+++ 18_graphs/hashingQues.cc
@@ -2,6 +2,7 @@
 // deepak@codingblocks.com
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <map>
 using namespace std;
 
@@ -30,6 +31,7 @@ vector<int> removeDuplicates(vector<int> v) {
     return ans;
 }
 
+/*****************************************************/
 
 // heapsort
 int parent(int i) { return (i - 1) / 2;}
@@ -66,6 +68,55 @@ void heapsort(vector<int>& v){
     }
 }
 
+/*****************************************************/
+
+class compareIntAsc{
+public:
+    bool operator()(int a, int b){
+        return a > b;
+    }
+};
+
+vector<int> bestk(vector<int> v, int k){
+    // finds the largest 3 elements
+
+    priority_queue<int, vector<int>, compareIntAsc> q;
+
+    // push first k elements in the heap
+    for(int i = 0; i < k; ++i){
+        q.push(v[i]);
+    }
+
+    // push remaining elements in the heap one by one
+    for(int i = k; i < v.size(); ++i){
+        q.push(v[i]);
+        q.pop();
+    }
+
+    // push the elements in the vector
+    vector<int> ans;
+    while(q.empty() == false){
+        ans.push_back(q.top());
+        q.pop();
+    }
+    return ans;
+}
+/*****************************************************/
+
+vector<int> bestk2(vector<int> v, int k){
+    make_heap(v.begin(), v.end());
+    // printVec(v);
+
+    vector<int> ans;
+    for(int i = 0; i < k; ++i){
+        ans.push_back(v[0]);
+        pop_heap(v.begin(), v.end() - i);
+    }
+    return ans;
+}
+
+
+/*****************************************************/
 int main() {
     vector<int> v;
     int n;
@@ -80,9 +131,9 @@ int main() {
     // vector<int> ans = removeDuplicates(v);
     // printVec(ans);
 
-    heapsort(v);
-    printVec(v);
+    // heapsort(v);
+    // printVec(v);
 
-
-
+    int k; cin >> k;
+    printVec(bestk2(v, k));
 }
